abbb3a012dd028e63d4e6b73a3c386655595d1d2
Rishabh <crishabh505@gmail.com>
2020-01-12 17:11:50 +0530

    init1

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export class App extends Component {
         },
         {
             id: '1',
-            name: 'The Shremad Bhagwat',
+            name: 'Shremad Bhagwat Purana',
             logo: Logo4,
             discription: "Originally composed in Sanskrit, this most studied, popular, revered, and influential Purana is an epic Vaishnava poem consisting of 18,000 shlokas (or verses) over 12 skandhas (or cantos). Its interconnected and interwoven narratives, teachings, and explanations focus on the forms (or avatars) of Vishnu particularly Krishna as the ultimate, primeval, transcendental source of the multiverse (including the demigods and gods such as Vishnu)  as well as the lives of his greatest devotees.......",
             wikipedia: 'https://en.wikipedia.org/wiki/Bhagavata_Purana'
@@ -36,19 +36,22 @@ export class App extends Component {
             id: '2',
             name: 'The Geeta',
             logo: Logo3,
-            discription: ""
+            discription: "The Gita is set in a narrative framework of a dialogue between Pandava prince Arjuna and his guide and charioteer Krishna. At the start of the Dharma Yudhha (righteous war) between Pandavas and Kauravas, Arjuna is filled with moral dilemma and despair about the violence and death the war will cause. He wonders if he should renounce and seeks Krishna's counsel, whose answers and discourse constitute the Bhagavad Gita.......",
+            wikipedia: 'https://en.wikipedia.org/wiki/Bhagavad_Gita'
         },
         {
             id: '3',
-            name: 'The Mahabharat',
+            name: 'The Mahabharata',
             logo: Logo2,
-            discription: ""
+            discription: "The Mahābhārata Sanskrit: महाभारतम्, Mahābhāratam, is one of the two major Sanskrit epics of ancient India, the other being the Rāmāyaṇa. It narrates the struggle between two groups of cousins in the Kurukshetra War and the fates of the Kaurava and the Pandava princes and their successors........",
+            wikipedia: 'https://en.wikipedia.org/wiki/Mahabharata'
         },
         {
             id: '4',
             name: 'Kalki Purana',
             logo: Logo5,
-            discription: ""
+            discription: "The Sri Kalki Purana is a prophetic work in Sanskrit that details the life and times of Kalki, the tenth and final of the Dashavatara (the ten Avatars) of the Hindu deity Lord Vishnu. The narrative is set in near the end of the Kali Yuga or Dark Age, as revealed by the storyteller Suta.........",
+            wikipedia: 'https://en.wikipedia.org/wiki/Kalki_Purana'
         },
         {
             id: '5',
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,22 +6,35 @@ export class Modal extends Component {
     super(props)
     this.state = {
         likebuttonlogo: './heart.svg',
-        downloadbuttonlogo: './down-arrow.svg'
+        downloadbuttonlogo: './down-arrow.svg',
+        x: 0
       
     }
 }
+
 changelikeStyle = (event) => {
   if (this.state.likebuttonlogo == './heart.svg') {
-    this.setState({likebuttonlogo: './heart-red.svg'})
+    this.setState({likebuttonlogo: './heart-red.svg', x: this.state.x + 1})
   }
   else {
-    this.setState({likebuttonlogo: './heart.svg'})
+    this.setState({likebuttonlogo: './heart.svg', x: this.state.x - 1})
   }
+}
+
+likeCalculator = () => {
+    if (this.state.x>=1000 ) {
+      this.state.x = this.state.x/1000 + 'k'
+    }
+    else {
+      this.state.x = this.state.x
+    }
+    return this.state.x
 }
 changedownloadStyle = (event) => {
   this.setState({downloadbuttonlogo: 'download.svg'})
 }
   render() {
+    console.log(this.likeCalculator())
     return (
       <>
       {this.props.selectedBook && (
@@ -39,7 +52,8 @@ changedownloadStyle = (event) => {
                   <p className="book-details">{this.props.selectedBook.discription}<a href={this.props.selectedBook.wikipedia}>Wikipedia</a></p>
                   </div>
                 <div className="row ab">
-                  <img className="col-3 likebutton" src={this.state.likebuttonlogo} alt="Like" onClick={(e) => this.changelikeStyle(e) }></img>
+                 <img className="col-3 likebutton" src={this.state.likebuttonlogo} alt="Like" onClick={(e) => this.changelikeStyle(e) }></img>
+                   <p>{this.state.x}</p>
                   <img className="col-3 sharebutton" src='./share.svg' alt="Like"></img>
                   <img className="col-3 downloadbutton" src={this.state.downloadbuttonlogo} alt="Download" onClick={(e) => this.changedownloadStyle(e) }></img>
                   <div className="lang">
