4325efd45ca49317303bd60d43512f9ab8a1510a
Rishabh <crishabh505@gmail.com>
2020-01-14 19:50:19 +0530

    init4

--- src/App.js
+++ src/App.js
@@ -23,109 +23,136 @@ export class App extends Component {
             name: 'The Ramayana',
             logo: Logo1,
             discription: "The epic, traditionally ascribed to the Maharshi Valmiki, narrates the life of Ram, the legendary prince of the Koshal Kingdom. It follows his fourteen-year exile to the forest by his father King Dasharatha, on request of his step-mother Kaikeyi. His travels across forests in India with his wife Seeta and brother Lakshman, the kidnapping of his wife by Ravana, the great king of Lanka, resulting in a war with him, and Ram's eventual return to Ayodhya to be crowned king is the crux of the epic.......",
-            wikipedia: 'https://en.wikipedia.org/wiki/Ramayana'
+            wikipedia: 'https://en.wikipedia.org/wiki/Ramayana',
+            catagory: 'mahakavya'
         },
         {
             id: '1',
             name: 'Shremad Bhagwat Purana',
             logo: Logo4,
             discription: "Originally composed in Sanskrit, this most studied, popular, revered, and influential Purana is an epic Vaishnava poem consisting of 18,000 shlokas (or verses) over 12 skandhas (or cantos). Its interconnected and interwoven narratives, teachings, and explanations focus on the forms (or avatars) of Vishnu particularly Krishna as the ultimate, primeval, transcendental source of the multiverse (including the demigods and gods such as Vishnu)  as well as the lives of his greatest devotees.......",
-            wikipedia: 'https://en.wikipedia.org/wiki/Bhagavata_Purana'
+            wikipedia: 'https://en.wikipedia.org/wiki/Bhagavata_Purana',
+            catagory: 'purana'
         },
         {
             id: '2',
             name: 'The Geeta',
             logo: Logo3,
             discription: "The Gita is set in a narrative framework of a dialogue between Pandava prince Arjuna and his guide and charioteer Krishna. At the start of the Dharma Yudhha (righteous war) between Pandavas and Kauravas, Arjuna is filled with moral dilemma and despair about the violence and death the war will cause. He wonders if he should renounce and seeks Krishna's counsel, whose answers and discourse constitute the Bhagavad Gita.......",
-            wikipedia: 'https://en.wikipedia.org/wiki/Bhagavad_Gita'
+            wikipedia: 'https://en.wikipedia.org/wiki/Bhagavad_Gita',
+            catagory: 'mahakavya'
         },
         {
             id: '3',
             name: 'The Mahabharata',
             logo: Logo2,
             discription: "The Mahābhārata Sanskrit: महाभारतम्, Mahābhāratam, is one of the two major Sanskrit epics of ancient India, the other being the Rāmāyaṇa. It narrates the struggle between two groups of cousins in the Kurukshetra War and the fates of the Kaurava and the Pandava princes and their successors........",
-            wikipedia: 'https://en.wikipedia.org/wiki/Mahabharata'
+            wikipedia: 'https://en.wikipedia.org/wiki/Mahabharata',
+            catagory: 'mahakavya'
         },
         {
             id: '4',
             name: 'Kalki Purana',
             logo: Logo5,
             discription: "The Sri Kalki Purana is a prophetic work in Sanskrit that details the life and times of Kalki, the tenth and final of the Dashavatara (the ten Avatars) of the Hindu deity Lord Vishnu. The narrative is set in near the end of the Kali Yuga or Dark Age, as revealed by the storyteller Suta.........",
-            wikipedia: 'https://en.wikipedia.org/wiki/Kalki_Purana'
+            wikipedia: 'https://en.wikipedia.org/wiki/Kalki_Purana',
+            catagory: 'purana'
         },
         {
             id: '5',
             name: 'Vishnu Purana',
             logo: Logo6,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'purana'
         },
         {
             id: '6',
             name: 'Garuda Purana',
             logo: Logo7,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'purana'
         },
         {
             id: '7',
             name: 'Shiv Purana',
             logo: Logo8,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'purana'
         },
         {
             id: '8',
             name: 'Rig Veda',
             logo: Logo9,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'veda'
         },
         {
             id: '9',
             name: 'Saam Veda',
             logo: Logo10,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'veda'
         },
         {
             id: '10',
             name: 'Yajur Veda',
             logo: Logo11,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'veda'
         },
         {
             id: '11',
             name: 'Atharv Veda',
             logo: Logo12,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'veda'
         },
         {
             id: '12',
             name: 'Matasya Purana',
             logo: Logo13,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'purana'
         },
         {
             id: '13',
             name: 'Agni Purana',
             logo: Logo14,
-            discription: ""
+            discription: "",
+            wikipedia: '',
+            catagory: 'purana'
         },
     ]
 
     const items = [
         {
             id: '0',
-            name: 'Vedas'
+            name: 'Vedas',
+            catagory: 'veda'
             
         },
         {
             id: '1',
-            name: 'Puranas'
+            name: 'Puranas',
+            catagory: 'purana'
         },
         {
             id: '2',
-            name: 'Mahakavyas'
+            name: 'Mahakavyas',
+            catagory: 'mahakavya'
         },
         {
             id: '3',
-            name: 'Test Object'
+            name: 'Others',
+            catagory: 'other'
         },
     ]
     return (
--- src/components/Navblock.js
+++ src/components/Navblock.js
@@ -14,19 +14,11 @@ export class Navblock extends Component {
                event.target.value.toLowerCase()) !== -1;
            })})
      }
-     updatedCatagory = (event) => {
-        this.setState({bookitems: this.state.initialItems.filter(function(item){
-            return item.name.toLowerCase().search(
-              event.target.catagory.toLowerCase()) !== -1;
-          })})
-    }
      UNSAFE_componentWillMount(){
          this.setState({bookitems: this.state.initialItems})
        }
-     catagory = []
     render() {
-       
-        console.log(this.catagory)
+
         
         return (
             <>
@@ -36,10 +28,9 @@ export class Navblock extends Component {
             <input type="text" placeholder="Search..." className="container inbox" onChange={(e) => this.updatedList(e)}></input>
                 {
                         this.props.items.map((items) => {
-                            this.catagory.push(items.name.substr(0, items.name.length - 1))
                             return (
                             <div key={items.id} className="row" title={items.name}>
-                             <div className="container catagory"  onClick = {(e) => {this.updatedCatagory(e)}}>
+                             <div className="container catagory" >
                             <h4><b>{items.name}</b></h4>
                             <p>&nbsp;Original Sanskrit copy</p>
                             </div>
